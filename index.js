ddc1aa276718d6bf2320f01e0f214fbb9c50ed90
Vidar Eldøy <vidar@eldoy.com>
2019-08-10 12:42:54 +0200

    Sending works

--- index.js
+++ index.js
@@ -1,4 +1,32 @@
 const mailgun = require('mailgun.js')
-const config = require('./config.js')
+const DEFAULT_CONFIG = require('./config.js')
+const ALIASES = [{ reply: 'h:Reply-To' }]
 
-console.log(config)
+// mail object looks like this:
+// {
+//   to: 'vidar@eldoy.com',
+//   from: 'vidar@fugroup.net',
+//   cc: 'cc@fugroup.net',
+//   bcc: 'bcc@fugroup.net',
+//   subject: 'hello',
+//   html: '<h1>Hello</h1>',
+//   text: 'Hello',
+//   reply: 'vidar@eldoy.com',
+//   attachment: [readStream],
+//   inline: [readStream]
+// }
+
+module.exports = function(mail, customConfig = {}) {
+  for (const pair of ALIASES) {
+    for (const key in pair) {
+      const val = pair[key]
+      if (mail[key]) {
+        mail[val] = mail[key]
+        delete mail[key]
+      }
+    }
+  }
+  const config = Object.assign({}, DEFAULT_CONFIG, customConfig)
+  const mg = mailgun.client({ username: 'api', key: config.key })
+  return mg.messages.create(config.domain, mail)
+}
--- test/mail.spec.js
+++ test/mail.spec.js
@@ -1,7 +1,26 @@
 const wmail = require('../index.js')
+const fs = require('fs')
+const fspath = require('path')
+const filename = 'sirloin.png'
+const filepath = fspath.join(__dirname, filename)
+// const file = fs.readFileSync(filepath)
+const file = fs.createReadStream(filepath)
 
 describe('wmail', () => {
-  it('should send an email', async () => {
-
+  it('should send an email with attachment', async () => {
+    const mail = {
+      to: 'Vidar Eldøy <vidar@eldoy.com>',
+      from: 'Fugroup <vidar@fugroup.net>',
+      cc: 'cc@fugroup.net',
+      bcc: 'bcc@fugroup.net',
+      subject: 'hello',
+      html: '<h1>Helloæøå</h1>',
+      text: 'Helloæøå',
+      reply: 'vidar@fugroup.net',
+      attachment: [file]
+    }
+    const result = await wmail(mail)
+    expect(result.id).toBeDefined()
+    expect(result.message).toBe('Queued. Thank you.')
   })
 })
